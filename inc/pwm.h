39f0ca902c5d262a0948cdaeaca3613429ec3ea6
PrajaktaZode <76157085+PrajaktaZode@users.noreply.github.com>
2021-07-26 23:19:01 +0530

    Create pwm.h

---
+++ inc/pwm.h
@@ -0,0 +1,13 @@
+#ifndef __PWM_H__
+#define __PWM_H__
+#include<avr/io.h>
+
+/**
+ * @brief Function definition
+ *
+ * @param temp_value
+ */
+
+void pwm(uint16_t temp_value);
+void InitPWM(void);
+#endif
